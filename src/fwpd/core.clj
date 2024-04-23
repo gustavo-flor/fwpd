73dd8d5a1b2ce997bdbf18b372596e153b60b1a5
Gustavo Flôr <ogustaflor@gmail.com>
2024-04-23 07:49:21 -0300

    :beers: FWPD for the Brave and True

--- src/fwpd/core.clj
+++ src/fwpd/core.clj
@@ -32,11 +32,53 @@
        rows))
 
 (defn glitter-filter
-  [minimum-glitter records]
+  [records minimum-glitter]
   (filter #(>= (:glitter-index %) minimum-glitter) records))
+
+(defn get-vamp-names
+  [vamps]
+  (map #(:name %) vamps))
+
+(defn is-valid-glitter-index?
+  [value]
+  (and ((complement nil?) value) (integer? value) (>= value 0) (<= value 10)))
+
+(def not-empty? (complement empty?))
+
+(def validations {:name not-empty?
+                  :glitter-index is-valid-glitter-index?})
+
+(defn is-valid?
+  [vamp-key value]
+  ((get validations vamp-key) value))
+
+(defn is-valid-vamp?
+  [vamp]
+  (and (is-valid? :name (:name vamp))
+       (is-valid? :glitter-index (:glitter-index vamp))))
+
+(defn append
+  [vamps vamp]
+  (if (is-valid-vamp? vamp)
+    (conj vamps vamp)
+    (identity vamps)))
+
+(defn vamp-keys-to-string
+  [vamp]
+  (str (:name vamp) "," (:glitter-index vamp)))
+
+(defn write-vamps-csv
+  [vamps]
+  (spit "vamps.csv" (clojure.string/join "\n" (map vamp-keys-to-string vamps))))
 
 (defn -main
   "I don't do a lot ... yet."
-  [& args]
-  (doseq [vamp (glitter-filter 3 (mapify (parse (slurp filename))))]
-    (println vamp)))
+  [& _]
+  (let [vamps (-> (slurp filename)
+                  parse
+                  mapify
+                  (append {:name "Gustavo Flôr" :glitter-index 0})
+                  (glitter-filter 3))]
+    (write-vamps-csv vamps)
+    (doseq [name (get-vamp-names vamps)]
+      (println name))))
